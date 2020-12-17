0c8d32b456419c68b800c58c053a5dcfcf4f261f
ABDESSAMAD ELOUALIALAMI <42350345+abdessamadalami@users.noreply.github.com>
2020-12-17 15:41:45 +0100

    Add files via upload

---
+++ Minimum-Swaps-2.js
@@ -0,0 +1,27 @@
+function minimumSwaps(arr) {
+
+ 
+   
+        var i,c=0,n=arr.length;
+        
+            for(i=0;i<n;i++)
+        
+            {
+              var k=0;
+             // console.log(i);
+                if(arr[i]!=(i+1))
+                  {
+        
+                    var b=arr[arr[i]-1];
+                    var a=arr[i];      
+                    arr[i]=b;
+                    arr[b] = a;    
+                    c++;
+                
+                   i--;
+                  }       
+            }
+            return c;
+        
+         
+        }
